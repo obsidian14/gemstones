773db582244651b9d3a0433685b664825fc1e226
obsidian14 <nonewu24@outlook.com>
2025-07-28 18:51:23 +0800

    true

--- src/build/core/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/phobos/Projects/gemstones/src/core
-# Build directory: /home/phobos/Projects/gemstones/src/build/core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[all_test]=] "/home/phobos/Projects/gemstones/src/build/core/all_test")
-set_tests_properties([=[all_test]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/phobos/Projects/gemstones/src/core/CMakeLists.txt;14;add_test;/home/phobos/Projects/gemstones/src/core/CMakeLists.txt;0;")
